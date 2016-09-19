c98acacaf1355bd50e8d0c91fb3ba15cb0a9c9cb
Stephane Gerber <gerber.stephane@gmail.com>
2016-09-19 12:17:22 +0200

    ok

--- index.php
+++ index.php
@@ -4,183 +4,183 @@ if ( php_sapi_name() !== 'cli') {
     echo 'Only cli';die;
 }
 require 'vendor/autoload.php';
-$allConfig = array();
+$allAccounts = array();
 
 // elesdyzer303
-$config = array();
-$config['name'] = 'elesdyzer303';
-$config['key'] = 'AKIAJADEQPQTKAQSMOFQ';
-$config['secret'] = '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';
-$config['region'] = 'us-west-2';
-$config['ami'] = 'ami-6ef72a0e';
-$allConfig[] = $config;
+$account = array();
+$account['name'] = 'elesdyzer303';
+$account['key'] = 'AKIAJADEQPQTKAQSMOFQ';
+$account['secret'] = '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';
+$account['region'] = 'us-west-2';
+$account['ami'] = 'ami-6ef72a0e';
+$allAccounts[] = $account;
 
 // artakan303
-$config = array();
-$config['name'] = 'artakan303';
-$config['key'] = 'AKIAJLK2MD6H7QFG2W2A';
-$config['secret'] = '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';
-$config['region'] = 'us-west-2';
-$config['ami'] = 'ami-e7fa2787';
-$allConfig[] = $config;
+$account = array();
+$account['name'] = 'artakan303';
+$account['key'] = 'AKIAJLK2MD6H7QFG2W2A';
+$account['secret'] = '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';
+$account['region'] = 'us-west-2';
+$account['ami'] = 'ami-e7fa2787';
+$allAccounts[] = $account;
 
 // artakan304
-$config = array();
-$config['name'] = 'artakan304';
-$config['key'] = 'AKIAJR6DHDKXOR4QP75Q';
-$config['secret'] = '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';
-$config['region'] = 'us-west-2';
-$config['ami'] = 'ami-8cf72aec';
-$allConfig[] = $config;
+$account = array();
+$account['name'] = 'artakan304';
+$account['key'] = 'AKIAJR6DHDKXOR4QP75Q';
+$account['secret'] = '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';
+$account['region'] = 'us-west-2';
+$account['ami'] = 'ami-8cf72aec';
+$allAccounts[] = $account;
 
 // artakan305
-$config = array();
-$config['name'] = 'artakan305';
-$config['key'] = 'AKIAJUGANS4IHZB5U6LA';
-$config['secret'] = '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';
-$config['region'] = 'us-west-2';
-$config['ami'] = 'ami-18ed3078';
-$allConfig[] = $config;
+$account = array();
+$account['name'] = 'artakan305';
+$account['key'] = 'AKIAJUGANS4IHZB5U6LA';
+$account['secret'] = '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';
+$account['region'] = 'us-west-2';
+$account['ami'] = 'ami-18ed3078';
+$allAccounts[] = $account;
 
 
 // artakan306
-$config = array();
-$config['name'] = 'artakan306';
-$config['key'] = 'AKIAI5RH7JAMGGDWKQJQ';
-$config['secret'] = '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';
-$config['region'] = 'us-west-2';
-$config['ami'] = 'ami-43f22f23';
-$allConfig[] = $config;
+$account = array();
+$account['name'] = 'artakan306';
+$account['key'] = 'AKIAI5RH7JAMGGDWKQJQ';
+$account['secret'] = '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';
+$account['region'] = 'us-west-2';
+$account['ami'] = 'ami-43f22f23';
+$allAccounts[] = $account;
 
 // artakan10
-$config = array();
-$config['name'] = 'artakan10';
-$config['key'] = 'AKIAJFGLKCXG75KYXVWQ';
-$config['secret'] = '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';
-$config['region'] = 'us-west-2';
-$config['ami'] = 'ami-5bec313b';
-$allConfig[] = $config;
+$account = array();
+$account['name'] = 'artakan10';
+$account['key'] = 'AKIAJFGLKCXG75KYXVWQ';
+$account['secret'] = '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';
+$account['region'] = 'us-west-2';
+$account['ami'] = 'ami-5bec313b';
+$allAccounts[] = $account;
 
 // artakan11 onze
-$config = array();
-$config['name'] = 'artakan11';
-$config['key'] = 'AKIAIGJGXOJRTTENFPAQ';
-$config['secret'] = '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';
-$config['region'] = 'us-west-2';
-$config['ami'] = 'ami-c4ed30a4';
-$allConfig[] = $config;
+$account = array();
+$account['name'] = 'artakan11';
+$account['key'] = 'AKIAIGJGXOJRTTENFPAQ';
+$account['secret'] = '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';
+$account['region'] = 'us-west-2';
+$account['ami'] = 'ami-c4ed30a4';
+$allAccounts[] = $account;
 
 
 // artakan12
-$config = array();
-$config['name'] = 'artakan12';
-$config['key'] = 'AKIAJBOBRBNG6N47YTEA';
-$config['secret'] = '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';
-$config['region'] = 'us-west-2';
-$config['ami'] = 'ami-75f22f15';
-$allConfig[] = $config;
+$account = array();
+$account['name'] = 'artakan12';
+$account['key'] = 'AKIAJBOBRBNG6N47YTEA';
+$account['secret'] = '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';
+$account['region'] = 'us-west-2';
+$account['ami'] = 'ami-75f22f15';
+$allAccounts[] = $account;
 
 
 // artakan13
-$config = array();
-$config['name'] = 'artakan13';
-$config['key'] = 'AKIAIJ5BABYVSDOLDQOQ';
-$config['secret'] = '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';
-$config['region'] = 'us-west-2';
-$config['ami'] = 'ami-6ef72a0e';
-$allConfig[] = $config;
+$account = array();
+$account['name'] = 'artakan13';
+$account['key'] = 'AKIAIJ5BABYVSDOLDQOQ';
+$account['secret'] = '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';
+$account['region'] = 'us-west-2';
+$account['ami'] = 'ami-6ef72a0e';
+$allAccounts[] = $account;
 
 
 // artakan14
-$config = array();
-$config['name'] = 'artakan14';
-$config['key'] = 'AKIAIRTUNVESOXFWS7LQ';
-$config['secret'] = '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';
-$config['region'] = 'us-west-2';
-$config['ami'] = 'ami-6ef72a0e';
-$allConfig[] = $config;
+$account = array();
+$account['name'] = 'artakan14';
+$account['key'] = 'AKIAIRTUNVESOXFWS7LQ';
+$account['secret'] = '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';
+$account['region'] = 'us-west-2';
+$account['ami'] = 'ami-6ef72a0e';
+$allAccounts[] = $account;
 
 
 // artakan15
-$config = array();
-$config['name'] = 'artakan15';
-$config['key'] = 'AKIAJUCSYERRFFALFF3Q';
-$config['secret'] = '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';
-$config['region'] = 'us-west-2';
-$config['ami'] = 'ami-6ef72a0e';
-$allConfig[] = $config;
+$account = array();
+$account['name'] = 'artakan15';
+$account['key'] = 'AKIAJUCSYERRFFALFF3Q';
+$account['secret'] = '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';
+$account['region'] = 'us-west-2';
+$account['ami'] = 'ami-6ef72a0e';
+$allAccounts[] = $account;
 
 // artakan16
-$config = array();
-$config['name'] = 'artakan16';
-$config['key'] = 'AKIAJHSFN5R6FHPO3WLA';
-$config['secret'] = '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';
-$config['region'] = 'us-west-2';
-$config['ami'] = 'ami-6ef72a0e';
-$allConfig[] = $config;
+$account = array();
+$account['name'] = 'artakan16';
+$account['key'] = 'AKIAJHSFN5R6FHPO3WLA';
+$account['secret'] = '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';
+$account['region'] = 'us-west-2';
+$account['ami'] = 'ami-6ef72a0e';
+$allAccounts[] = $account;
 
 // artakan17
-$config = array();
-$config['name'] = 'artakan17';
-$config['key'] = 'AKIAJ6HQGX6JQCQGCJVA';
-$config['secret'] = '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';
-$config['region'] = 'us-west-2';
-$config['ami'] = 'ami-e7fa2787';
-$allConfig[] = $config;
+$account = array();
+$account['name'] = 'artakan17';
+$account['key'] = 'AKIAJ6HQGX6JQCQGCJVA';
+$account['secret'] = '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';
+$account['region'] = 'us-west-2';
+$account['ami'] = 'ami-e7fa2787';
+$allAccounts[] = $account;
 
 // artakan18
-$config = array();
-$config['name'] = 'artakan18';
-$config['key'] = 'AKIAINMZGINL7EIATWPA';
-$config['secret'] = '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';
-$config['region'] = 'us-west-2';
-$config['ami'] = 'ami-43f22f23';
-$allConfig[] = $config;
+$account = array();
+$account['name'] = 'artakan18';
+$account['key'] = 'AKIAINMZGINL7EIATWPA';
+$account['secret'] = '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';
+$account['region'] = 'us-west-2';
+$account['ami'] = 'ami-43f22f23';
+$allAccounts[] = $account;
 
 // artakan19
-$config = array();
-$config['name'] = 'artakan19';
-$config['key'] = 'AKIAILS5A6GIMKK3LIMQ';
-$config['secret'] = '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';
-$config['region'] = 'us-west-2';
-$config['ami'] = 'ami-6ef72a0e';
-$allConfig[] = $config;
+$account = array();
+$account['name'] = 'artakan19';
+$account['key'] = 'AKIAILS5A6GIMKK3LIMQ';
+$account['secret'] = '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';
+$account['region'] = 'us-west-2';
+$account['ami'] = 'ami-6ef72a0e';
+$allAccounts[] = $account;
 
 // artakan20
-$config = array();
-$config['name'] = 'artakan20';
-$config['key'] = 'AKIAJ34ZMNPNV4SRSWJA';
-$config['secret'] = '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';
-$config['region'] = 'us-west-2';
-$config['ami'] = 'ami-6ef72a0e';
-$allConfig[] = $config;
+$account = array();
+$account['name'] = 'artakan20';
+$account['key'] = 'AKIAJ34ZMNPNV4SRSWJA';
+$account['secret'] = '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';
+$account['region'] = 'us-west-2';
+$account['ami'] = 'ami-6ef72a0e';
+$allAccounts[] = $account;
 
 // artakan21
-$config = array();
-$config['name'] = 'artakan21';
-$config['key'] = 'AKIAISOKW4VO7NLAUAXQ';
-$config['secret'] = '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';
-$config['region'] = 'us-west-2';
-$config['ami'] = 'ami-e7fa2787';
-$allConfig[] = $config;
+$account = array();
+$account['name'] = 'artakan21';
+$account['key'] = 'AKIAISOKW4VO7NLAUAXQ';
+$account['secret'] = '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';
+$account['region'] = 'us-west-2';
+$account['ami'] = 'ami-e7fa2787';
+$allAccounts[] = $account;
 
 // artakan22
-$config = array();
-$config['name'] = 'artakan22';
-$config['key'] = 'AKIAJTUQNRD4GGFIO2QQ';
-$config['secret'] = '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';
-$config['region'] = 'us-west-2';
-$config['ami'] = 'ami-e7fa2787';
-$allConfig[] = $config;
+$account = array();
+$account['name'] = 'artakan22';
+$account['key'] = 'AKIAJTUQNRD4GGFIO2QQ';
+$account['secret'] = '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';
+$account['region'] = 'us-west-2';
+$account['ami'] = 'ami-e7fa2787';
+$allAccounts[] = $account;
 
 // artakan23
-$config = array();
-$config['name'] = 'artakan23';
-$config['key'] = 'AKIAJF3B7OK3V2CJ33UA';
-$config['secret'] = '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';
-$config['region'] = 'us-west-2';
-$config['ami'] = 'ami-8cf72aec';
-$allConfig[] = $config;
+$account = array();
+$account['name'] = 'artakan23';
+$account['key'] = 'AKIAJF3B7OK3V2CJ33UA';
+$account['secret'] = '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';
+$account['region'] = 'us-west-2';
+$account['ami'] = 'ami-8cf72aec';
+$allAccounts[] = $account;
 
 
 function getInstances ($ec2Client) {
@@ -198,57 +198,51 @@ function getInstances ($ec2Client) {
 }
 
 
-foreach($allConfig as $config){
-    $credentials = new \Aws\Credentials\Credentials($config['key'], $config['secret']);
-    $ec2Client = \Aws\Ec2\Ec2Client::factory(array(
-        'credentials' => $credentials,
-        'region' => $config['region'],
-        'scheme' => 'http',
-        'version' => '2016-04-01'
-    ));
+foreach($allAccounts as &$account){
 
-    $instances = getInstances($ec2Client);
+    $account['client'] = 
+        \Aws\Ec2\Ec2Client::factory(array(
+           'credentials' =>  new \Aws\Credentials\Credentials($account['key'], $account['secret']),
+           'region' => $account['region'],
+           'scheme' => 'http',
+           'version' => '2016-04-01'
+       ));
+
+    $instances = getInstances($account['client']);
 
     if (!empty($instances)){
-        $res = $ec2Client->terminateInstances(array(
+        $res = $account['client']->terminateInstances(array(
             'InstanceIds' => $instances // REQUIRED
         ));
-        echo($config['name'].' termination'.PHP_EOL);
+        echo($account['name'].' termination'.PHP_EOL);
     }
 }
-foreach($allConfig as $config){
+foreach($allAccounts as $account){
 
-    echo($config['name'].' starting'.PHP_EOL);
-    $credentials = new \Aws\Credentials\Credentials($config['key'], $config['secret']);
-    $ec2Client = \Aws\Ec2\Ec2Client::factory(array(
-        'credentials' => $credentials,
-        'region' => $config['region'],
-        'scheme' => 'http',
-        'version' => '2016-04-01'
-    ));
+    echo($account['name'].' starting'.PHP_EOL);
 
-    $instances = getInstances($ec2Client);
+    $instances = getInstances($account['client']);
     if (!empty($instances)) {
-        echo 'Waiting for termination of '.$config['name'].PHP_EOL;
-        $ec2Client->waitUntil('InstanceTerminated', ['InstanceIds' => $instances]);
+        echo 'Waiting for termination of '.$account['name'].PHP_EOL;
+        $account['client']->waitUntil('InstanceTerminated', ['InstanceIds' => $instances]);
         echo (count($instances).' instance terminated'.PHP_EOL);
     }
     try {
-        $res =$ec2Client->runInstances(array(
-            'ImageId' => $config['ami'], // REQUIRED
+        $res =$account['client']->runInstances(array(
+            'ImageId' => $account['ami'], // REQUIRED
             'MaxCount' => 20, // REQUIRED
             'MinCount' => 20, // REQUIRED
             'InstanceType' => 't2.micro'
         ));
-        echo($config['name'].' started'.PHP_EOL);
+        echo($account['name'].' started'.PHP_EOL);
     }
     catch (Exception $ex) {
         echo $ex->getMessage().PHP_EOL;
 	echo 'Exception:Try again in 3 minutes....'.PHP_EOL;
         sleep (180);
         try {
-                $res =$ec2Client->runInstances(array(
-                'ImageId' => $config['ami'], // REQUIRED
+                $res =$account['client']->runInstances(array(
+                'ImageId' => $account['ami'], // REQUIRED
                 'MaxCount' => 20, // REQUIRED
                 'MinCount' => 20, // REQUIRED
                 'InstanceType' => 't2.micro'
