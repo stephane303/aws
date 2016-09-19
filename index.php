f180f71bd3018247bf9a1b50f2fecb6f9879abdd
Stephane Gerber <gerber.stephane@gmail.com>
2016-09-19 12:09:44 +0200

    ok

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+echo date("F j, Y, g:i a").PHP_EOL; 
+if ( php_sapi_name() !== 'cli') {
+    echo 'Only cli';die;
+}
 require 'vendor/autoload.php';
 $allConfig = array();
 
@@ -10,8 +14,7 @@ $config['secret'] = 'RLQq5rg1R9eknbDEFhu/q5oVw8hav4oo8zi791p4';
 $config['region'] = 'us-west-2';
 $config['ami'] = 'ami-6ef72a0e';
 $allConfig[] = $config;
-    
-    
+
 // artakan303
 $config = array();
 $config['name'] = 'artakan303';
@@ -76,44 +79,184 @@ $config['secret'] = 'gBab1dGR4pr27a2gh+M2KwmXNLvKdp67AW2mv94H';
 $config['region'] = 'us-west-2';
 $config['ami'] = 'ami-75f22f15';
 $allConfig[] = $config;
-  
+
+
+// artakan13
+$config = array();
+$config['name'] = 'artakan13';
+$config['key'] = 'AKIAIJ5BABYVSDOLDQOQ';
+$config['secret'] = '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';
+$config['region'] = 'us-west-2';
+$config['ami'] = 'ami-6ef72a0e';
+$allConfig[] = $config;
+
+
+// artakan14
+$config = array();
+$config['name'] = 'artakan14';
+$config['key'] = 'AKIAIRTUNVESOXFWS7LQ';
+$config['secret'] = '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';
+$config['region'] = 'us-west-2';
+$config['ami'] = 'ami-6ef72a0e';
+$allConfig[] = $config;
+
+
+// artakan15
+$config = array();
+$config['name'] = 'artakan15';
+$config['key'] = 'AKIAJUCSYERRFFALFF3Q';
+$config['secret'] = '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';
+$config['region'] = 'us-west-2';
+$config['ami'] = 'ami-6ef72a0e';
+$allConfig[] = $config;
+
+// artakan16
+$config = array();
+$config['name'] = 'artakan16';
+$config['key'] = 'AKIAJHSFN5R6FHPO3WLA';
+$config['secret'] = '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';
+$config['region'] = 'us-west-2';
+$config['ami'] = 'ami-6ef72a0e';
+$allConfig[] = $config;
+
+// artakan17
+$config = array();
+$config['name'] = 'artakan17';
+$config['key'] = 'AKIAJ6HQGX6JQCQGCJVA';
+$config['secret'] = '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';
+$config['region'] = 'us-west-2';
+$config['ami'] = 'ami-e7fa2787';
+$allConfig[] = $config;
+
+// artakan18
+$config = array();
+$config['name'] = 'artakan18';
+$config['key'] = 'AKIAINMZGINL7EIATWPA';
+$config['secret'] = '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';
+$config['region'] = 'us-west-2';
+$config['ami'] = 'ami-43f22f23';
+$allConfig[] = $config;
+
+// artakan19
+$config = array();
+$config['name'] = 'artakan19';
+$config['key'] = 'AKIAILS5A6GIMKK3LIMQ';
+$config['secret'] = '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';
+$config['region'] = 'us-west-2';
+$config['ami'] = 'ami-6ef72a0e';
+$allConfig[] = $config;
+
+// artakan20
+$config = array();
+$config['name'] = 'artakan20';
+$config['key'] = 'AKIAJ34ZMNPNV4SRSWJA';
+$config['secret'] = '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';
+$config['region'] = 'us-west-2';
+$config['ami'] = 'ami-6ef72a0e';
+$allConfig[] = $config;
+
+// artakan21
+$config = array();
+$config['name'] = 'artakan21';
+$config['key'] = 'AKIAISOKW4VO7NLAUAXQ';
+$config['secret'] = '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';
+$config['region'] = 'us-west-2';
+$config['ami'] = 'ami-e7fa2787';
+$allConfig[] = $config;
+
+// artakan22
+$config = array();
+$config['name'] = 'artakan22';
+$config['key'] = 'AKIAJTUQNRD4GGFIO2QQ';
+$config['secret'] = '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';
+$config['region'] = 'us-west-2';
+$config['ami'] = 'ami-e7fa2787';
+$allConfig[] = $config;
+
+// artakan23
+$config = array();
+$config['name'] = 'artakan23';
+$config['key'] = 'AKIAJF3B7OK3V2CJ33UA';
+$config['secret'] = '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';
+$config['region'] = 'us-west-2';
+$config['ami'] = 'ami-8cf72aec';
+$allConfig[] = $config;
+
+
 function getInstances ($ec2Client) {
     $arrayOfInstances = array();
-    $result = $ec2Client->DescribeInstances(array());
-
-    $reservations = $result['Reservations'];
-    foreach ($reservations as $reservation) {
+    $reservations = $ec2Client->getIterator('DescribeInstances', array());
+    foreach($reservations as $reservation){
         $instances = $reservation['Instances'];
-        foreach ($instances as $instance) {
-            $arrayOfInstances[] = $instance['InstanceId'];
+        foreach($instances as $instance){
+            $arrayOfInstances[]=$instance['InstanceId'];
         }
     }
+
     return $arrayOfInstances;
+
 }
 
-echo '<pre>';
+
 foreach($allConfig as $config){
     $credentials = new \Aws\Credentials\Credentials($config['key'], $config['secret']);
     $ec2Client = \Aws\Ec2\Ec2Client::factory(array(
         'credentials' => $credentials,
         'region' => $config['region'],
         'scheme' => 'http',
-        'version' => 'latest'
-    ));    
+        'version' => '2016-04-01'
+    ));
 
     $instances = getInstances($ec2Client);
 
-    $res = $ec2Client->terminateInstances(array(
-        'InstanceIds' => $instances // REQUIRED         
-    ));
-    $ec2Client->waitUntil('InstanceTerminated', ['InstanceIds' => $instances]);
+    if (!empty($instances)){
+        $res = $ec2Client->terminateInstances(array(
+            'InstanceIds' => $instances // REQUIRED
+        ));
+        echo($config['name'].' termination'.PHP_EOL);
+    }
+}
+foreach($allConfig as $config){
 
-    $res =$ec2Client->runInstances(array(
-        'ImageId' => $config['ami'], // REQUIRED   
-        'MaxCount' => 20, // REQUIRED
-        'MinCount' => 20, // REQUIRED    
-        'InstanceType' => 't2.micro'
+    echo($config['name'].' starting'.PHP_EOL);
+    $credentials = new \Aws\Credentials\Credentials($config['key'], $config['secret']);
+    $ec2Client = \Aws\Ec2\Ec2Client::factory(array(
+        'credentials' => $credentials,
+        'region' => $config['region'],
+        'scheme' => 'http',
+        'version' => '2016-04-01'
     ));
-    
-    echo($config['name'].' started');PHP_EOL;
+
+    $instances = getInstances($ec2Client);
+    if (!empty($instances)) {
+        echo 'Waiting for termination of '.$config['name'].PHP_EOL;
+        $ec2Client->waitUntil('InstanceTerminated', ['InstanceIds' => $instances]);
+        echo (count($instances).' instance terminated'.PHP_EOL);
+    }
+    try {
+        $res =$ec2Client->runInstances(array(
+            'ImageId' => $config['ami'], // REQUIRED
+            'MaxCount' => 20, // REQUIRED
+            'MinCount' => 20, // REQUIRED
+            'InstanceType' => 't2.micro'
+        ));
+        echo($config['name'].' started'.PHP_EOL);
+    }
+    catch (Exception $ex) {
+        echo $ex->getMessage().PHP_EOL;
+	echo 'Exception:Try again in 3 minutes....'.PHP_EOL;
+        sleep (180);
+        try {
+                $res =$ec2Client->runInstances(array(
+                'ImageId' => $config['ami'], // REQUIRED
+                'MaxCount' => 20, // REQUIRED
+                'MinCount' => 20, // REQUIRED
+                'InstanceType' => 't2.micro'
+            ));
+        }
+        catch(Exception $ex){
+            echo $ex->getMessage().PHP_EOL;
+            echo 'Exception:Did not work'.PHP_EOL;
+        }
+    }
 }
