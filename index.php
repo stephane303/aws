889248fda68b53bd46333927fac23858046d3818
Stephane Gerber <gerber.stephane@gmail.com>
2016-09-19 13:36:33 +0200

    ok

--- index.php
+++ index.php
@@ -4,199 +4,8 @@ if ( php_sapi_name() !== 'cli') {
     echo 'Only cli';die;
 }
 require 'vendor/autoload.php';
-$allAccounts = array();
-
-// elesdyzer303
-$account = array();
-$account['name'] = 'elesdyzer303';
-$account['key'] = 'AKIAJADEQPQTKAQSMOFQ';
-$account['secret'] = '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';
-$account['region'] = 'us-west-2';
-$account['ami'] = 'ami-6ef72a0e';
-$allAccounts[] = $account;
-
-// artakan303
-$account = array();
-$account['name'] = 'artakan303';
-$account['key'] = 'AKIAJLK2MD6H7QFG2W2A';
-$account['secret'] = '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';
-$account['region'] = 'us-west-2';
-$account['ami'] = 'ami-e7fa2787';
-$allAccounts[] = $account;
-
-// artakan304
-$account = array();
-$account['name'] = 'artakan304';
-$account['key'] = 'AKIAJR6DHDKXOR4QP75Q';
-$account['secret'] = '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';
-$account['region'] = 'us-west-2';
-$account['ami'] = 'ami-8cf72aec';
-$allAccounts[] = $account;
-
-// artakan305
-$account = array();
-$account['name'] = 'artakan305';
-$account['key'] = 'AKIAJUGANS4IHZB5U6LA';
-$account['secret'] = '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';
-$account['region'] = 'us-west-2';
-$account['ami'] = 'ami-18ed3078';
-$allAccounts[] = $account;
-
-
-// artakan306
-$account = array();
-$account['name'] = 'artakan306';
-$account['key'] = 'AKIAI5RH7JAMGGDWKQJQ';
-$account['secret'] = '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';
-$account['region'] = 'us-west-2';
-$account['ami'] = 'ami-43f22f23';
-$allAccounts[] = $account;
-
-// artakan10
-$account = array();
-$account['name'] = 'artakan10';
-$account['key'] = 'AKIAJFGLKCXG75KYXVWQ';
-$account['secret'] = '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';
-$account['region'] = 'us-west-2';
-$account['ami'] = 'ami-5bec313b';
-$allAccounts[] = $account;
-
-// artakan11 onze
-$account = array();
-$account['name'] = 'artakan11';
-$account['key'] = 'AKIAIGJGXOJRTTENFPAQ';
-$account['secret'] = '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';
-$account['region'] = 'us-west-2';
-$account['ami'] = 'ami-c4ed30a4';
-$allAccounts[] = $account;
-
-
-// artakan12
-$account = array();
-$account['name'] = 'artakan12';
-$account['key'] = 'AKIAJBOBRBNG6N47YTEA';
-$account['secret'] = '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';
-$account['region'] = 'us-west-2';
-$account['ami'] = 'ami-75f22f15';
-$allAccounts[] = $account;
-
-
-// artakan13
-$account = array();
-$account['name'] = 'artakan13';
-$account['key'] = 'AKIAIJ5BABYVSDOLDQOQ';
-$account['secret'] = '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';
-$account['region'] = 'us-west-2';
-$account['ami'] = 'ami-6ef72a0e';
-$allAccounts[] = $account;
-
-
-// artakan14
-$account = array();
-$account['name'] = 'artakan14';
-$account['key'] = 'AKIAIRTUNVESOXFWS7LQ';
-$account['secret'] = '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';
-$account['region'] = 'us-west-2';
-$account['ami'] = 'ami-6ef72a0e';
-$allAccounts[] = $account;
-
-
-// artakan15
-$account = array();
-$account['name'] = 'artakan15';
-$account['key'] = 'AKIAJUCSYERRFFALFF3Q';
-$account['secret'] = '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';
-$account['region'] = 'us-west-2';
-$account['ami'] = 'ami-6ef72a0e';
-$allAccounts[] = $account;
-
-// artakan16
-$account = array();
-$account['name'] = 'artakan16';
-$account['key'] = 'AKIAJHSFN5R6FHPO3WLA';
-$account['secret'] = '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';
-$account['region'] = 'us-west-2';
-$account['ami'] = 'ami-6ef72a0e';
-$allAccounts[] = $account;
-
-// artakan17
-$account = array();
-$account['name'] = 'artakan17';
-$account['key'] = 'AKIAJ6HQGX6JQCQGCJVA';
-$account['secret'] = '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';
-$account['region'] = 'us-west-2';
-$account['ami'] = 'ami-e7fa2787';
-$allAccounts[] = $account;
-
-// artakan18
-$account = array();
-$account['name'] = 'artakan18';
-$account['key'] = 'AKIAINMZGINL7EIATWPA';
-$account['secret'] = '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';
-$account['region'] = 'us-west-2';
-$account['ami'] = 'ami-43f22f23';
-$allAccounts[] = $account;
-
-// artakan19
-$account = array();
-$account['name'] = 'artakan19';
-$account['key'] = 'AKIAILS5A6GIMKK3LIMQ';
-$account['secret'] = '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';
-$account['region'] = 'us-west-2';
-$account['ami'] = 'ami-6ef72a0e';
-$allAccounts[] = $account;
-
-// artakan20
-$account = array();
-$account['name'] = 'artakan20';
-$account['key'] = 'AKIAJ34ZMNPNV4SRSWJA';
-$account['secret'] = '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';
-$account['region'] = 'us-west-2';
-$account['ami'] = 'ami-6ef72a0e';
-$allAccounts[] = $account;
-
-// artakan21
-$account = array();
-$account['name'] = 'artakan21';
-$account['key'] = 'AKIAISOKW4VO7NLAUAXQ';
-$account['secret'] = '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';
-$account['region'] = 'us-west-2';
-$account['ami'] = 'ami-e7fa2787';
-$allAccounts[] = $account;
-
-// artakan22
-$account = array();
-$account['name'] = 'artakan22';
-$account['key'] = 'AKIAJTUQNRD4GGFIO2QQ';
-$account['secret'] = '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';
-$account['region'] = 'us-west-2';
-$account['ami'] = 'ami-e7fa2787';
-$allAccounts[] = $account;
-
-// artakan23
-$account = array();
-$account['name'] = 'artakan23';
-$account['key'] = 'AKIAJF3B7OK3V2CJ33UA';
-$account['secret'] = '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';
-$account['region'] = 'us-west-2';
-$account['ami'] = 'ami-8cf72aec';
-$allAccounts[] = $account;
-
-
-function getInstances ($ec2Client) {
-    $arrayOfInstances = array();
-    $reservations = $ec2Client->getIterator('DescribeInstances', array());
-    foreach($reservations as $reservation){
-        $instances = $reservation['Instances'];
-        foreach($instances as $instance){
-            $arrayOfInstances[]=$instance['InstanceId'];
-        }
-    }
-
-    return $arrayOfInstances;
-
-}
-
+require 'config.php';
+require 'lib.php';
 
 foreach($allAccounts as &$account){
 
@@ -207,50 +16,20 @@ foreach($allAccounts as &$account){
            'scheme' => 'http',
            'version' => '2016-04-01'
        ));
-
-    $instances = getInstances($account['client']);
-
-    if (!empty($instances)){
-        $res = $account['client']->terminateInstances(array(
-            'InstanceIds' => $instances // REQUIRED
-        ));
-        echo($account['name'].' termination'.PHP_EOL);
-    }
+       terminateAllInstances($account);
 }
+
+foreach($allAccounts as &$account){
+    startAllInstances($account);
+}
+
+// Traitement des accounts en erreur
+echo 'Traitement des erreurs:'.PHP_EOL;
 foreach($allAccounts as $account){
-
-    echo($account['name'].' starting'.PHP_EOL);
-
-    $instances = getInstances($account['client']);
-    if (!empty($instances)) {
-        echo 'Waiting for termination of '.$account['name'].PHP_EOL;
-        $account['client']->waitUntil('InstanceTerminated', ['InstanceIds' => $instances]);
-        echo (count($instances).' instance terminated'.PHP_EOL);
-    }
-    try {
-        $res =$account['client']->runInstances(array(
-            'ImageId' => $account['ami'], // REQUIRED
-            'MaxCount' => 20, // REQUIRED
-            'MinCount' => 20, // REQUIRED
-            'InstanceType' => 't2.micro'
-        ));
-        echo($account['name'].' started'.PHP_EOL);
-    }
-    catch (Exception $ex) {
-        echo $ex->getMessage().PHP_EOL;
-	echo 'Exception:Try again in 3 minutes....'.PHP_EOL;
-        sleep (180);
-        try {
-                $res =$account['client']->runInstances(array(
-                'ImageId' => $account['ami'], // REQUIRED
-                'MaxCount' => 20, // REQUIRED
-                'MinCount' => 20, // REQUIRED
-                'InstanceType' => 't2.micro'
-            ));
-        }
-        catch(Exception $ex){
-            echo $ex->getMessage().PHP_EOL;
-            echo 'Exception:Did not work'.PHP_EOL;
-        }
+    if (isset($account['Exception'])) {
+        // On refait tous le binz
+        terminateAllAccountInstances($account );  
+        startAllInstances($account);        
     }
 }
+
---
+++ lib.php
@@ -0,0 +1,52 @@
+<?php
+function getInstances ($ec2Client) {
+    $arrayOfInstances = array();
+    $reservations = $ec2Client->getIterator('DescribeInstances', array());
+    foreach($reservations as $reservation){
+        $instances = $reservation['Instances'];
+        foreach($instances as $instance){
+            $arrayOfInstances[]=$instance['InstanceId'];
+        }
+    }
+
+    return $arrayOfInstances;
+
+}
+
+function terminateAllInstances ($account){
+    $instances = getInstances($account['client']);
+
+    if (!empty($instances)){
+        $res = $account['client']->terminateInstances(array(
+            'InstanceIds' => $instances 
+        ));
+        echo($account['name'].' termination'.PHP_EOL);
+    }    
+}
+
+
+function startAllInstances (&$account){
+    echo($account['name'].' starting'.PHP_EOL);
+
+    $instances = getInstances($account['client']);
+    if (!empty($instances)) {
+        echo 'Waiting for termination of '.$account['name'].PHP_EOL;
+        $account['client']->waitUntil('InstanceTerminated', ['InstanceIds' => $instances]);
+        echo (count($instances).' instance terminated'.PHP_EOL);
+    }
+    try {
+        $res =$account['client']->runInstances(array(
+            'ImageId' => $account['ami'], 
+            'MaxCount' => 20, 
+            'MinCount' => 20, 
+            'InstanceType' => 't2.micro'
+        ));
+        echo($account['name'].' started'.PHP_EOL);
+    }
+    catch (Exception $ex) {
+        echo $ex->getMessage().PHP_EOL;
+	echo 'Exception:Try again later....'.PHP_EOL;
+        $account['Exception'] = true;
+    }    
+}
+
